8b8f1dfc8f92c5a173b44477497b55f1f3cf777f
Priyanshu Shrivastav <official.mr.convict@gmail.com>
2024-09-09 10:29:18 +0530

    chore: Fixes ts issues

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-c/onst list = [1, 2, 3].map(x => x + 1);
+const list = [1, 2, 3].map((x) => x + 1);
 console.log(list);
 
 type Item = string | number;
 const mutateItems = (items: Array<Item>): void => {
   items.push("Hello world");
-}
+};
 
 const numbers: Array<number> = [1, 2, 3];
 
