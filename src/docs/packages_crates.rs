29bb62eb2f05aef2c10b4c575944e5cf8882b5ae
Priyanshu Shrivastav <official.mr.convict@gmail.com>
2025-05-30 00:26:20 +0530

    docs: Revisit packages and crates

--- src/docs/packages_crates.rs
+++ src/docs/packages_crates.rs
@@ -17,6 +17,7 @@ pub mod back_of_house {
 
     impl Breakfast {
         pub fn summer(toast_name: &str) -> Breakfast {
+            // static method as it doesn't take self
             Breakfast {
                 toast: String::from(toast_name),
                 seasonal_fruit: String::from("Apple"),
@@ -25,23 +26,30 @@ pub mod back_of_house {
     }
 }
 
-// Re-exporting as different name,
+// Re-exporting as different name
+// (in rust terms, public re-export with an alias, or also known as API forwarding)
 pub use back_of_house::Appetizer as HouseAppetizer;
+
+/* NOTE: use -> importing a name into local namespace
+ * pub use -> re-exporting that name from your module, so others can import */
 
 pub fn eat_at_rest() {
     let breakfast = back_of_house::Breakfast::summer("brown");
     println!("{:?}", breakfast);
     // println!("{}", breakfast.seasonal_fruit) // this will break since seasonal_fruit is a private field
 
-    let initial_order = back_of_house::Appetizer::Soup;
+    // let initial_order = back_of_house::Appetizer::Soup; // OR
+    let initial_order = HouseAppetizer::Soup;
     println!("{:?}", initial_order);
 }
 
 /*
 // use are scoped
-use back_of_house::Breakfast;
-mod some_mod {
-   // here you cannot use Breakfast directly
+use back_of_house::Breakfast; // local namespace
+
+mod some_mod { // some_mod namespace
+   // here you cannot use Breakfast directly, because Breakfast is valid in local namespace but not
+   // some_mod namespace
 }
 */
 
