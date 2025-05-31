5b6ada0d42e07cab25194afff72f2904d0595cf3
Priyanshu Shrivastav <official.mr.convict@gmail.com>
2025-06-01 03:40:57 +0530

    docs: Starts with smart pointers

--- src/docs/mod.rs
+++ src/docs/mod.rs
@@ -3,3 +3,4 @@ pub mod guessing_game;
 pub mod lifetimes;
 pub mod match_docs;
 pub mod packages_crates;
+pub mod smart_pointers;
---
+++ src/docs/smart_pointers.rs
@@ -0,0 +1,24 @@
+// Smart pointers
+/* Rust already has & (references, borrows the value they point to)
+ * Smart pointers have additional metadata and functionalities over & and pointers
+ * - references just borrow values, smart pointers in most cases owns the value they point to.
+ * Eg. String and Vec<_>
+ *
+ * Box<T> -> lightest smart pointer, storing value of type T, keeping it in heap. only the pointer stays on
+ * the stack. No performance overhead. Useful when:
+ *
+ * - size not known at compile time, but use a value of that type in a context where you need the
+ * exact size (like recursive type)
+ * - large amount of data, but don't want to copy the data when transfering the ownership
+ * - when you want to own a value, but all you can is that type implements certain traits rather
+ * than being of a specific type (also known as trait object) (? ToDo: need more explanation)
+ *
+ * Box implements Deref, and Drop traits -- useful as smart pointers (ToDo: More on this later!)
+ * */
+
+pub fn check() {
+    let b = Box::new(4);
+    // after b's scope end, both the the pointer (on the stack) and the value (on the heap) are dropped
+
+    println!("{:?}", *b);
+}
