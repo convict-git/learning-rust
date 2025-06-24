c5e5793f353bacc6112f4905d4a44914066e639a
Priyanshu Shrivastav <official.mr.convict@gmail.com>
2025-06-25 02:55:33 +0530

    docs: Dont let futures starve using yields; abstractions like timeout and retry with timeout

--- src/docs/async_docs.rs
+++ src/docs/async_docs.rs
@@ -129,11 +129,12 @@ mod async_docs {
         future::Future,
         pin::{pin, Pin},
         sync::Arc,
+        thread,
         time::Duration,
     };
     use tokio::{
         sync::{mpsc::unbounded_channel, Mutex},
-        task::spawn as spawn_task,
+        task::{spawn as spawn_task, yield_now as async_yield_now},
         time::sleep as async_sleep,
     };
 
@@ -251,5 +252,117 @@ mod async_docs {
         });
 
         assert_eq!(data, vec![0, 6, 1, 7, 2, 8, 3, 9, 4, 5]); // NOTE: This could be still a flaky test
+    }
+
+    #[test]
+    fn yield_now_dont_let_us_starve() {
+        let slow_closure = || thread::sleep(Duration::from_millis(1000));
+        // ^ imitating a sync blocking slow job
+
+        let race_result = helpers::tokio_rt_block_on(async {
+            let fut_a = async {
+                slow_closure(); // block the thread with a slow blocking task
+                async_yield_now().await; // regular yeild ensure that other futures are not
+                                         // starving due the long-running slow blocking operations
+                                         // in one of the futures. This is done by manually yielding
+                                         // the control back to runtime to allow other futures to run
+                slow_closure();
+                async_yield_now().await;
+                slow_closure();
+                async_yield_now().await;
+                slow_closure();
+            };
+
+            let fut_b = async {
+                slow_closure();
+            };
+
+            helpers::race(fut_a, fut_b).await
+        });
+        if let Either::Left(_) = race_result {
+            panic!("First future can't win the race");
+            // NOTE: Uncomment the async_yield_now().await in fut_a to see fut_b never getting the
+            // resource to win the race and starve
+        }
+    }
+
+    #[test]
+    fn abstractions_with_futures_custom_timeout() {
+        // Implementing custom async timeout function which returns a Result<Future::Output, Duration>
+        async fn custom_timeout<F: Future>(
+            f: F,
+            max_time: Duration,
+        ) -> Result<F::Output, Duration> {
+            match helpers::race(f, async_sleep(max_time)).await {
+                Either::Left(o) => Ok(o),
+                Either::Right(_) => Err(max_time),
+            }
+        }
+
+        let slow_fut = async {
+            async_sleep(Duration::from_millis(2000)).await;
+            return 0;
+        };
+
+        let fast_fut = async {
+            async_sleep(Duration::from_millis(100)).await;
+            return 1;
+        };
+
+        helpers::tokio_rt_block_on(async {
+            // Slow future should timeout and return Result::Err
+            assert!(custom_timeout(slow_fut, Duration::from_millis(200))
+                .await
+                .is_err());
+
+            // Fast future should run correctly and return Result::Ok
+            assert!(custom_timeout(fast_fut, Duration::from_millis(200))
+                .await
+                .is_ok());
+        });
+    }
+
+    #[test]
+    fn abstraction_with_futures_custom_future_retry_with_timeouts() {
+        // Todo: Keeping this iterative, though we can keep it recursive, but that needs some Box
+        // pinning since future cannot be infinitely sized.. Will come back to this later
+        async fn retry_with_timeout<F: Future, FutGenClosure: Fn() -> F>(
+            fut_generation_closure: FutGenClosure,
+            max_time: Duration,
+            max_tries: u8,
+        ) -> Result<(F::Output, u8), ()> {
+            let mut current_tries = 0;
+            loop {
+                current_tries += 1;
+                if current_tries > max_tries {
+                    break;
+                }
+
+                // Need a fut_generation_closure since we a need a fresh future for every iteration.
+                // This is because helpers::race will move the future and will make it unusable for further iterations
+                let fut = fut_generation_closure();
+                if let Either::Left(o) = helpers::race(fut, async_sleep(max_time)).await {
+                    return Result::Ok((o, current_tries));
+                }
+            }
+
+            Result::Err(())
+        }
+
+        let get_a_random_slow_future = || async {
+            let random_delay = rand::thread_rng().gen_range(1..=200);
+            async_sleep(Duration::from_millis(random_delay)).await;
+            42
+        };
+
+        helpers::tokio_rt_block_on(async {
+            // NOTE: This is a flaky test but just for learning purposes, assuming that atleast in
+            // 10 random tries, one of the slow_future will run in less than 50ms.
+            assert!(
+                retry_with_timeout(get_a_random_slow_future, Duration::from_millis(50), 10)
+                    .await
+                    .is_ok()
+            );
+        });
     }
 }
